d80941fc4c88e046ec01ed87ace850c9cea739ca
dogexpert90 <dogexpert900@outlook.com>
2025-08-10 13:18:45 +0300

    flio

--- gallery/fileslist.js
+++ gallery/fileslist.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-// Juurkaust kust pildid tulevad
+// Juurkaust, kust pildid tulevad
 const baseDir = path.join(__dirname, 'assets', 'wallpapers');
 const outputFile = path.join(__dirname, 'files.js');
 
@@ -9,17 +9,18 @@ const outputFile = path.join(__dirname, 'files.js');
 const imageExtensions = /\.(jpe?g|png|gif|webp|bmp)$/i;
 
 // Rekursiivne failide kogumine
-function getAllImages(dirPath, basePath = dirPath) {
+function getAllImages(dirPath) {
   let results = [];
-
   const files = fs.readdirSync(dirPath);
+
   files.forEach(file => {
     const filePath = path.join(dirPath, file);
     const stat = fs.statSync(filePath);
 
     if (stat.isDirectory()) {
-      results = results.concat(getAllImages(filePath, basePath));
+      results = results.concat(getAllImages(filePath));
     } else if (imageExtensions.test(file)) {
+      // Tee suhteliseks (__dirname suhtes), asenda Windowsi kaldkriipsud
       const relativePath = path.relative(__dirname, filePath).replace(/\\/g, '/');
       results.push(relativePath);
     }
@@ -30,9 +31,17 @@ function getAllImages(dirPath, basePath = dirPath) {
 
 const images = getAllImages(baseDir);
 
-// Genereeri JS-fail
+// Genereeri JS-fail, korrektselt template stringina
 const content = `// Automaatselt genereeritud
-const images = [\n  ${images.map(p => `"${p}"`).join(',\n  ')}\n];\n`;
 
-fs.writeFileSync(outputFile, content);
+const images = [
+  ${images.map(p => `"${p}"`).join(',\n  ')}
+];
+
+module.exports = images;
+`;
+
+// Kirjuta fail
+fs.writeFileSync(outputFile, content, 'utf-8');
+
 console.log(`✅ files.js genereeritud (${images.length} faili).`);
