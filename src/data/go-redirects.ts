e2554154b0660ea35c04777fe563621c0ce9e267
dogexpert900 <dogexpert900@outlook.com>
2026-07-22 11:43:36 +0300

    fix: remove outdated notice, fix broken go redirects, update blog styling

--- src/data/go-redirects.ts
+++ src/data/go-redirects.ts
@@ -4,16 +4,12 @@ export const goRedirects: Record<string, string> = {
   youtubeextra: 'https://www.youtube.com/@DogExpertExtra/',
   discord: 'https://discord.gg/a7F7p6S63J',
   HvUTdUi:
-    'https://ia601008.us.archive.org/24/items/ddu-v-18.1.3.1/DDU%20v18.1.3.1.exe',
+    'https://www.wagnardsoft.com/displayddu',
   ADBAMD29: 'https://www.amd.com/en/support/download/drivers.html',
   GWIN11ENVG:
     'https://www.microsoft.com/en-us/software-download/windows11',
   GWIN11ESVT:
     'https://www.microsoft.com/et-ee/software-download/windows11',
-  GSW732:
-    'https://drive.massgrave.dev/en_windows_7_ultimate_with_sp1_x86_dvd_u_677460.iso',
-  GSW764:
-    'https://drive.massgrave.dev/en_windows_7_ultimate_with_sp1_x64_dvd_u_677332.iso',
   GEW10BO10:
     'https://dn721905.ca.archive.org/0/items/bob-ombs-windows-10-pe-iso/Bob.Ombs.Modified.Win10PEx64.v4.985.ISO',
   G0PyRCD:
