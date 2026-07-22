10f0c0237d7c547e225baf7676045443f29a8951
dogexpert900 <dogexpert900@outlook.com>
2026-07-22 18:08:35 +0300

    Add Anti-Petya mirror page, clickable software list, video button, fix favicon
    - Anti-Petya go link now shows a mirror selection page (Archive.org, Google Drive, OneDrive)
    - Go redirect system supports both single URL (redirect) and array (mirror page)
    - About page: Software I Use items are now clickable cards linking to each app
    - About page: Removed Blender, replaced Adobe Photoshop with Paint.NET
    - Home page: Added 'View All Videos' button below video grid
    - Regenerated favicon.ico as proper multi-size ICO (16/32/48px, 14KB vs broken 655B)
    - Fixed favicon rel type from shortcut to image/x-icon for YouTube compatibility
    - Removed Windows ISOs downloads section from home page
    - Updated all Windows ISO go links to use buzzheavier mirrors from massgrave.dev

--- src/data/go-redirects.ts
+++ src/data/go-redirects.ts
@@ -1,39 +1,36 @@
-/** Short-link redirects under /go/ (see archive/go/ for originals). */
-export const goRedirects: Record<string, string> = {
+export interface MirrorLink {
+  label: string;
+  url: string;
+}
+
+export type GoRedirectValue = string | MirrorLink[];
+
+export const goRedirects: Record<string, GoRedirectValue> = {
   youtube: 'https://www.youtube.com/@dogexperttech/',
   youtubeextra: 'https://www.youtube.com/@DogExpertExtra/',
   discord: 'discord.gg/a7F7p6S63J',
-  HvUTdUi:
-    'https://www.wagnardsoft.com/displayddu',
+  HvUTdUi: 'https://www.wagnardsoft.com/displayddu',
   ADBAMD29: 'https://www.amd.com/en/support/download/drivers.html',
-  GWIN11ENVG:
-    'https://www.microsoft.com/en-us/software-download/windows11',
-  GWIN11ESVT:
-    'https://www.microsoft.com/et-ee/software-download/windows11',
+  GWIN11ENVG: 'https://www.microsoft.com/en-us/software-download/windows11',
+  GWIN11ESVT: 'https://www.microsoft.com/et-ee/software-download/windows11',
   GEW10BO10:
     'https://dn721905.ca.archive.org/0/items/bob-ombs-windows-10-pe-iso/Bob.Ombs.Modified.Win10PEx64.v4.985.ISO',
-  G0PyRCD:
-    'https://archive.org/download/antipetya_ultimate/antipetya_ultimate.iso',
+  G0PyRCD: [
+    { label: 'Archive.org', url: 'https://archive.org/download/antipetya_ultimate/antipetya_ultimate.iso' },
+    { label: 'Google Drive', url: 'https://drive.google.com/uc?id=1PH0YA-bYJXmGbuAl1Tcaqn2FvmGYX8cT&export=download' },
+    { label: 'OneDrive', url: 'https://1drv.ms/u/c/afe901c9cc84bf2b/IQCA7MZmslv6Qqplp49BJssVAR9YQyYtdFbxhTIiQjE7zr4?e=8wDNhp' },
+  ],
   GoAvL1: 'https://www.google.com',
   Dog10PE: '/Dog10PE/',
-  GWIN7X64:
-    'https://archive.org/download/windows-7-iso-image/windows_7_ultimate_with_sp1_x64_dvd_u_677031.iso',
-  GWIN7X86:
-    'https://archive.org/download/windows-7-iso-image/windows_7_ultimate_with_sp1_x86_dvd_u_677460.iso',
-  GWIN10X64:
-    'https://www.microsoft.com/en-us/software-download/windows10',
-  GWIN10X86:
-    'https://www.microsoft.com/en-us/software-download/windows10',
-  GWIN8X64:
-    'https://archive.org/download/Windows8/Windows8_Pro_English_x64.iso',
-  GWIN8X86:
-    'https://archive.org/download/Windows8/Windows8_Pro_English_x86.iso',
-  GWIN81X64:
-    'https://archive.org/download/win81iso/Windows%208.1%20Pro%20VL%20%28x64%29%20%28English%29.iso',
-  GWIN81X86:
-    'https://archive.org/download/win81iso/Windows%208.1%20Pro%20VL%20%28x86%29%20%28English%29.iso',
-  GWINXP:
-    'https://archive.org/download/WindowsXPProfessionalx64EN/Windows%20XP%20Professional%20x64%20Edition%20ENGLISH.iso',
+  GWIN7X64: 'https://buzzheavier.com/lllag2h2ucql',
+  GWIN7X86: 'https://buzzheavier.com/wru8ac6nwk2x',
+  GWIN10X64: 'https://buzzheavier.com/fuxscqu93mnn',
+  GWIN10X86: 'https://buzzheavier.com/4wlxz1qpf5vm',
+  GWIN8X64: 'https://buzzheavier.com/dn0bb0ozyxup',
+  GWIN8X86: 'https://buzzheavier.com/4uh1amj718hl',
+  GWIN81X64: 'https://buzzheavier.com/2i65axai8pao',
+  GWIN81X86: 'https://buzzheavier.com/ddhrb4eax1x1',
+  GWINXP: 'https://buzzheavier.com/nar2zwokpo9t',
 };
 
 export const goRedirectSlugs = Object.keys(goRedirects);
